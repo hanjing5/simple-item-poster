95ddbaa80d58ad6842ff7775b97af120aa97eeec
Han Jing <hanqijing@gmail.com>
2012-03-11 03:50:00 -0700

    deleted useless methods in companies controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -42,25 +42,4 @@ class CompaniesController < ApplicationController
   	@companies = Company.order("name DESC")
   end
 
-	def account
-		@company = current_company
-	end
-	
-	def update_password
-		@company = Company.find(current_company.id)
-		#if @company.password != params[:companies][:password_confirmation]
-		#	flash[:error] = 'Old password didnt match. Please check again.'
-		#	redirect_to account_companies_path
-		#	return
-		#end
-		if @company.update_attributes(params[:companies])
-			sign_in @company, :bypass=>true
-			flash[:success] = 'Success! Password changed!'
-			redirect_to account_companies_path
-		else
-			flash[:error] = 'Something went wrong. Please try again.'
-			redirect_to account_companies_path
-		end
-	end
-
 end
